4681c7c06318d2581ff263eb41eb4bf37672b027
Иван <vania.spongebob@gmail.com>
2017-04-09 12:16:28 +0300

    Добавил таблицу в проект и научился отображать элементы

---
+++ Курсовая/ChecklistViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Курсовая
+//
+//  Created by Иван on 07.04.17.
+//  Copyright © 2017 IvanCode. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Этот метод говорит TableView, сколько всего ячеек будет получено
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    //Этот метод даёт данные для TableView
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        //Получаем ячейку из таблицы (а точнее Label этой ячейки, т.к. её tag = 1000)
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        /*
+        //Здесь мы просто заполняем ячейки текстом (для каждой ячейки свой текст)
+        switch (indexPath.row) {
+            case 0: label.text = "Погулять с собакой"; break
+            case 1: label.text = "Поработать над курсачом"; break
+            case 2: label.text = "Помыть посуду"; break
+            case 3: label.text = "Сделать лабу"; break
+            case 4: label.text = "Купить кабель"; break
+            default: break
+        }
+        */
+        
+        if indexPath.row % 5 == 0 {
+            label.text = "Погулять с собакой"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Поработать над курсачом"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Помыть посуду"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Сделать лабу"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Купить кабель"
+        }
+        
+        return cell
+    }
+    
+}
+
