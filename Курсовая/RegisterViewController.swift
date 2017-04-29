f012d6c2ce0d8f20c845f6082c9c9c0b4c959a87
Иван <vania.spongebob@gmail.com>
2017-04-30 00:35:18 +0300

    Всё!)

--- Курсовая/RegisterViewController.swift
+++ Курсовая/RegisterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SignInViewController.swift
+//  RegisterViewController.swift
 //  Курсовая
 //
 //  Created by Иван on 10.04.17.
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 
+//Прячет клавиатуру по нажатию на пустое место на экране
 extension UIViewController {
     func hideKeyboardWhenTappedAround() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
@@ -21,7 +22,7 @@ extension UIViewController {
     }
 }
 
-class SignInViewController: UIViewController {
+class RegisterViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -31,82 +32,38 @@ class SignInViewController: UIViewController {
         super.viewDidLoad()
         
         //Делаем UITextField для ввода e-mail скруглённым, а также изменяем его рамку и цвет этой рамки
-        emailTextField.layer.cornerRadius = 15.0
-        emailTextField.layer.borderWidth = 2.0
-        emailTextField.layer.borderColor = UIColor.white.cgColor
-        
+        CustomizeUIElements.customizeBounds(element: emailTextField, color: UIColor.white.cgColor)
         //Делаем UITextField для ввода пароля скруглённым, а также изменяем его рамку и цвет этой рамки
-        passwordTextField.layer.cornerRadius = 15.0
-        passwordTextField.layer.borderWidth = 2.0
-        passwordTextField.layer.borderColor = UIColor.white.cgColor
-        
+        CustomizeUIElements.customizeBounds(element: passwordTextField, color: UIColor.white.cgColor)
         //Настраиваем нашу кнопку (делаем её скруглённой и зелёного цвета)
-        registerButton.layer.cornerRadius = 15.0
-        registerButton.layer.borderWidth = 2.0
-        registerButton.layer.borderColor = UIColor.init(red: 0, green: 128, blue: 0, alpha: 0).cgColor
+        CustomizeUIElements.customizeBounds(element: registerButton, color: UIColor.init(red: 0, green: 128, blue: 0, alpha: 0).cgColor)
         
         //Делаем так, чтобы клавиатура пряталась по нажатию на что-либо на экране
         self.hideKeyboardWhenTappedAround()
         
-        //Говорим, что пароль ещё не введён
-        //UserDefaults.standard.set(false, forKey: "loggedIn")
-        //UserDefaults.standard.synchronize()
-        
         //Очищаем поля ввода e-mail и пароля
         self.emailTextField.text = ""
         self.passwordTextField.text = ""
-    }
-    
-    //Функция, которая сохраняет на устройстве введённый пароль и e-mail
-    func savePasswordAndEmail() -> Void {
-        UserDefaults.standard.set(true, forKey: "loggedIn")
-        UserDefaults.standard.synchronize()
         
-        UserDefaults.standard.setValue(self.emailTextField.text, forKey: "userEmail")
-        UserDefaults.standard.synchronize()
-        
-        UserDefaults.standard.setValue(self.passwordTextField.text, forKey: "userPassword")
-        UserDefaults.standard.synchronize()
     }
     
     //Создаёт или входит в аккаунт пользователя по нажатию на Зарегистрироваться/Войти
     @IBAction func createAccount(sender: AnyObject) {
-        FIRAuth.auth()?.createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
-            //Если пользователь уже имеет аккаунт, то сразу перекидываем его в login
-            if error != nil {
-                UserDefaults.standard.set(false, forKey: "firstTime")
-                UserDefaults.standard.synchronize()
-                
-                print("Not first time")
-                
-                self.login()
-            //Если же пользователь создал только что аккаунт, то тоже перекидываем его в login
-            } else {
-                UserDefaults.standard.set(true, forKey: "firstTime")
-                UserDefaults.standard.synchronize()
-                
-                self.login()
-                print("User created!")
-            }
+        //Если есть соединение с интернетом
+        if Reachability.isConnectedToNetwork() == true {
+            //Регистрируем пользователя, заходим в его аккаунт и осуществляем переход на главный ViewController приложения
+            RegistrationAndLoginViaEmail.register(email: emailTextField.text!, password: passwordTextField.text!, viewController: self, identifier: "MyFirstTable")
         }
-    }
-    
-    //Обеспечивает регистрацию/вход в аккаунт пользователя
-    func login() {
-        FIRAuth.auth()?.signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
-            if error != nil {
-                print("Problems with signing in!");
-            } else {
-                print("Signed succesfully!")
-                self.savePasswordAndEmail()
-                
-                let user = FIRAuth.auth()?.currentUser?.uid
-                UserDefaults.standard.set(user, forKey: "UserID")
-                UserDefaults.standard.synchronize()
-                
-                //Выполняем переход в основной ViewControlle
-                self.performSegue(withIdentifier: "MyFirstTable", sender: self)
-            }
+        //Если же соединения с интернетом нет
+        else {
+            //Инициализируем наше сообщение об отсутствии интернета
+            let alert = UIAlertController(title: "Ошибка!", message: "Проверьте соединение с интернетом.", preferredStyle: .alert)
+            //Добавляем ему обработчик (в данном случае он пустой)
+            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+            //Устанавливаем обработчик
+            alert.addAction(action)
+            //Выводим сообщение
+            present(alert, animated: true, completion: nil)
         }
     }
     
@@ -115,6 +72,5 @@ class SignInViewController: UIViewController {
         //Осуществляет переход на ViewController со входом в аккаунт
         self.performSegue(withIdentifier: "WantToSignIn", sender: self)
     }
-    
 
 }
